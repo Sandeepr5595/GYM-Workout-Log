0e7d74b0e8b364e1d08916d7eabb68edc03cbcc4
Sandeepr5595 <Sandeepr5595@gmail.com>
2025-06-19 12:17:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  // ✅ Add this to fix relative asset loading in production
+  base: './',
 })
